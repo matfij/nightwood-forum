a47bc9955e7bc297d2db4a12f10b565ca922ae52
matfij <mateusz.michal.fijak@gmail.com>
2023-05-05 15:24:34 +0200

    configure auth db

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_HOST, DB_PASSWORD, DB_PORT, DB_TYPE, DB_USER } from './common/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: DB_TYPE,
+            host: DB_HOST,
+            port: DB_PORT,
+            username: DB_USER,
+            password: DB_PASSWORD,
+        }),
+    ],
 })
 export class AppModule {}
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { APP_PORT } from './common/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(14000);
-  console.log('auth-app started')
+    const app = await NestFactory.create(AppModule);
+    await app.listen(APP_PORT);
+    console.log('auth-app started');
 }
 bootstrap();
