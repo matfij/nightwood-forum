f566eaf0500ff818a69ffe15b0944a2c5da5f12c
matfij <mateusz.michal.fijak@gmail.com>
2023-08-05 17:02:35 +0200

    restore vite config

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -7,6 +7,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: true,
+        watch: {
+            usePolling: true,
+        },
+    },
     test: {
         globals: true,
         environment: 'jsdom',
